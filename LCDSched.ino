12484d4462e2db8b992060dfbf599bef6846f988
marcpouliot <marc.pouliot.1@gmail.com>
2016-10-04 08:08:00 -0400

    Version with messages

--- LCDSched.ino
+++ LCDSched.ino
@@ -1,27 +1,42 @@
-#include <TaskScheduler.h>
+/* 
+  Arduino 16x2 LCD with buttons example working with the Arkhipenko/TaskScheduler
+
+This is a very simple example on using the arkhipendo/TaskScheduler https://github.com/arkhipenko/TaskScheduler and the LCD/button shield. The intent is to comment the sketch file and understand how github works by testing pull requests.
+I assume a Branch is for major versions.
+Serial.print and println are used for debugging.
+2016 Marc Pouliot
+*/
+#include <TaskScheduler.h>  // library from https://github.com/arkhipenko/TaskScheduler
+// callback routines
 void tInputCallback();
 void tCommandCallback();
 void tTimerCallback();
-int adcToButtons();
 
-Task tInput(100,TASK_FOREVER,&tInputCallback);
-Task tCommand(200,1,&tCommandCallback);
-Task tTimer(1000,TASK_FOREVER,&tTimerCallback);
-Scheduler runner;
 
-#include <EEPROM.h>
+Task tInput(100,TASK_FOREVER,&tInputCallback);  // input from buttons
+Task tCommand(200,1,&tCommandCallback);         // issue the command
+Task tTimer(1000,TASK_FOREVER,&tTimerCallback); // free running timer 
+
+Scheduler runner;            // THE Scheduler
+
 //______________________________________
+
+#include <EEPROM.h>         // future use. check if this generates errors on compile
 #include <LiquidCrystal.h>
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);           // select the pins used on the LCD panel
 
-int lcd_key     = 0;
-int adValue  = 0;
-#define btnRIGHT  0
-#define btnUP     1
-#define btnDOWN   2
-#define btnLEFT   3
-#define btnSELECT 4
-#define btnNONE   5
+int LCDKey     = 0;
+int adValue  = 0;         // Analog to digital value, read on input.
+
+// NOTE : if backlit is used with this module, other a/d lines may not work properly,
+// ... I lifted pin 10 since I had problems reading consisetnt temperature sensor 
+
+#define BTNRIGHT  0
+#define BTNUP     1
+#define BTNDOWN   2
+#define BTNLEFT   3
+#define BTNSELECT 4
+#define BTNNONE   5
 
 byte smiley[8] = {
   B00000,
@@ -32,32 +47,41 @@ byte smiley[8] = {
   B01110,
   B00000,
 };
+// function definitions
+int adcToButtons();
+
 //______________________________________
+
 void setup() {
-   lcd.createChar(0, smiley);
+  lcd.createChar(0, smiley);
   lcd.begin(16, 2);               // start the library  COL ROW
   lcd.setCursor(0, 0);            // set the LCD cursor   position
-  lcd.print("Testing LCD Shed");  // print a simple message on the LCD
+  lcd.write(byte(0));             //smiley
+  stringToLCD(0,1,"Testing LCDShed",15);  // print a simple message on the LCD
   Serial.begin(115200);
   Serial.println("Scheduler TEST");
 
   runner.init();
-  runner.addTask(tInput);
+  
+  runner.addTask(tInput);     // read buttons
   tInput.enable();
+  
   runner.addTask(tTimer);
- 
-  tTimer.enableDelayed(3000);
-  runner.addTask(tCommand);
+  tTimer.enableDelayed(3000); // WAIT 3 seconds, start seconds timer
+
+  runner.addTask(tCommand);   // react to command
 
   }
 //______________________________________
+
 void loop() {
   // put your main code here, to run repeatedly:
-  runner.execute();
+  runner.execute();  // the scheduler greatly simplifies the loop and timing.
 }
 //______________________________________
+
 void tInputCallback(){
-lcd_key = adcToButtons();
+LCDKey = adcToButtons();
 Serial.print("input callB ");
 Serial.println((int)adValue);
 
@@ -65,6 +89,7 @@ tCommand.setIterations(1);
 tCommand.enable();
 }
 //______________________________________
+
 int adcToButtons() {              // read the buttons
   adValue = analogRead(0);       // read the value from the sensor
 
@@ -72,61 +97,58 @@ int adcToButtons() {              // read the buttons
   // we add approx 50 to those values and check to see if we are close
   // We make this the 1st option for speed reasons since it will be the most likely result
 
-  if (adValue > 1000) return btnNONE;
+  if (adValue > 1000) return BTNNONE;
 
   // For V1.1 us this threshold
-  if (adValue < 50)   return btnRIGHT;
-  if (adValue < 250)  return btnUP;
-  if (adValue < 450)  return btnDOWN;
-  if (adValue < 650)  return btnLEFT;
-  if (adValue < 850)  return btnSELECT;
-  return btnNONE;                // when all others fail, return this.
+  if (adValue < 50)   return BTNRIGHT;
+  if (adValue < 250)  return BTNUP;
+  if (adValue < 450)  return BTNDOWN;
+  if (adValue < 650)  return BTNLEFT;
+  if (adValue < 850)  return BTNSELECT;
+  return BTNNONE;                // when all others fail, return this.
 }
 //______________________________________
+
 void tCommandCallback()
 {
   lcd.setCursor(0, 1); 
   Serial.println("Cmd callB");
-  switch (lcd_key) {              // depending on which button was pushed, we perform an action
+  switch (LCDKey) {              // depending on which button was pushed, we perform an action
 
-    case btnRIGHT: {            //  push button "RIGHT" and show the word on the screen
+    case BTNRIGHT: {            //  push button "RIGHT" and show the word on the screen
         lcd.print("RIGHT ");
         break;
       }
-    case btnLEFT: {
+    case BTNLEFT: {
         lcd.print("LEFT   "); //  push button "LEFT" and show the word on the screen
         break;
       }
-    case btnUP: {
+    case BTNUP: {
         lcd.print("UP    ");  //  push button "UP" and show the word on the screen
         break;
     }
       
-    case btnDOWN: {
+    case BTNDOWN: {
         lcd.print("DOWN  ");  //  push button "DOWN" and show the word on the screen
         break;
       }
-    case btnSELECT: {
+    case BTNSELECT: {
         lcd.print("SELECT");  //  push button "SELECT" and show the word on the screen
         break;
       }
-    case btnNONE: {
+    case BTNNONE: {
         lcd.print("NONE  ");  //  No action  will show "None" on the screen
         break;
       }
   }
-//if (tCommand.isFirstIteration()){
-  //tTimer.enable();
-  //tCommand.delay(2000);
-  //stringToLCD(0,0,"FirstCommand    ",-1); 
-
-//}
-
 }
 //______________________________________
   
 void stringToLCD( int row,int col, String string, int blanks)
 {
+  // Blank and display the string at row, col (starts with 0.)
+  // If blanks = -1 use String length to erase characters beforehand
+  // otherwise erase just (number of) blanks spaces.
   int i;
   String blankStr="";
   lcd.setCursor(col,row); // initial call reverse logic.  should be row col.
@@ -141,7 +163,9 @@ void stringToLCD( int row,int col, String string, int blanks)
   lcd.print(string);
 }
 //______________________________________
+
 void tTimerCallback(){
+  // just add the seconds and display
 static int value=0;
   value+=1;
   Serial.println("Timer");
